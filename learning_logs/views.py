244d8fa520c35c9636c4cd6c39f546242a1e69ab
mattyocode <matthewoliver@live.co.uk>
2020-05-17 19:00:41 +0100

    Fresh start

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     '''The home page for Learning Log'''
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    '''show all topics'''
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learnings_logs/topics.html', context)
