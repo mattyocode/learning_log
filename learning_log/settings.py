9b96c3a8384efbc84e0e36758f42bc664ac6802c
mattyocode <matthewoliver@live.co.uk>
2020-05-25 15:17:35 +0100

    added bootstrap4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,10 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #My apps
+    # My apps.
     'learning_logs',
     'app_users',
-    #Default Django apps
+
+    # Third party apps.
+    'bootstrap4',
+
+    # Default Django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
